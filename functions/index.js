c5ac481d783cd2c9d8a6daf759db4105b0d46016
Gregory Petroski <gpetrosk@gmail.com>
2018-12-06 12:44:44 -0800

    Update to dialogflow v2

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 'use strict';
 
-process.env.DEBUG = 'actions-on-google:*';
-const App = require('actions-on-google').DialogflowApp;
+// // Import the Dialogflow module from the Actions on Google client library.
+const {dialogflow} = require('actions-on-google');
+
+// Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
+
+// Instantiate the Dialogflow client.
+const app = dialogflow({debug: true});
+
 const http = require('http');
 const xml2js = require('xml2js');
 const moment = require('moment-timezone');
@@ -18,31 +24,23 @@ const ROUTE = '18';
 const STOP = '564';
 const TIMEZONE = "America/Los_Angeles";
 
-exports.nextBusPrediction = functions.https.onRequest((request, response) => {
-    const app = new App({request, response});
-
-    function getBusTime(app) {
-        getNextBusResponse(function (response) {
-            app.tell(buildNextBusSentence(response));
-        });
-    }
-
-    function welcome(app) {
-        getNextBusResponse(function (response) {
-            app.tell(buildWelcomeSentence(response));
-        });
-    }
-
-    let actionMap = new Map();
-    actionMap.set(BUS_ACTION, getBusTime);
-    actionMap.set(WELCOME, welcome);
-
-    app.handleRequest(actionMap);
+app.intent(BUS_ACTION, (conv) => {
+    getNextBusResponse(function (response) {
+        conv.close(buildNextBusSentence(response));
+    });
 });
+
+app.intent(WELCOME, (conv) => {
+    getNextBusResponse(function (response) {
+        conv.close(buildWelcomeSentence(response));
+    });
+});
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
 
 function buildWelcomeSentence(response) {
     if (!response) {
-        return "Unable to communicate with next bus. Try again later";
+        return "There are no predictions from NextBus at this time. Try again later";
     } else {
         var sentence = "Hello. " + response.routeName + " stops at " + response.stopName +
             " headed toward " + response.direction + ". ";
@@ -52,6 +50,7 @@ function buildWelcomeSentence(response) {
 }
 
 function buildNextBusSentence(response) {
+    console.log("test");
     if (!response || response.predictions.length === 0) {
         return "There are no bus predictions at this time";
     } else {
@@ -95,32 +94,26 @@ function getNextBusResponse(responseCallback) {
                     console.log(err);
                     responseCallback();
                 } else {
-                    var predictions = result.body.predictions[0].direction[0].prediction;
-                    var routeInfo = result.body.predictions[0]['$'];
-                    response.routeName = routeInfo.routeTitle;
-                    response.stopName = routeInfo.stopTitle;
-                    response.direction = result.body.predictions[0].direction[0]['$'].title;
-                    for (var predInd in predictions) {
-                        var time = moment(Number(predictions[predInd]['$'].epochTime)).tz(TIMEZONE).format('h:mma');
-                        var minutes = predictions[predInd]['$'].minutes;
+                    if (!result.body.predictions) {
+                        var predictions = result.body.predictions[0].direction[0].prediction;
+                        var routeInfo = result.body.predictions[0]['$'];
+                        response.routeName = routeInfo.routeTitle;
+                        response.stopName = routeInfo.stopTitle;
+                        response.direction = result.body.predictions[0].direction[0]['$'].title;
+                        for (var predInd in predictions) {
+                            var time = moment(Number(predictions[predInd]['$'].epochTime)).tz(TIMEZONE).format('h:mma');
+                            var minutes = predictions[predInd]['$'].minutes;
 
-                        var prediction = {
-                            time: time,
-                            minutes: minutes
-                        };
-                        response.predictions.push(prediction);
+                            var prediction = {
+                                time: time,
+                                minutes: minutes
+                            };
+                            response.predictions.push(prediction);
+                        }
+                        responseCallback(response);
                     }
-                    responseCallback(response);
                 }
             });
         });
     }).end();
 }
-
-// getNextBusResponse(function(response) {
-//     console.log(buildNextBusSentence(response));
-// });
-//
-// getNextBusResponse(function(response) {
-//     console.log(buildWelcomeSentence(response));
-// });
